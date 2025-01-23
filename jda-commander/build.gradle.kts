842be9f59ef051e7adf64c40e88415f14a2fe6b2
Airdead <herbesbird@gmail.com>
2025-01-23 19:30:20 +0200

    Обновлен .gitignore и пересканированы файлы

---
+++ jda-commander/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    `maven-publish`
+}
+
+group = "xyz.yamida"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.2.2")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "yamidaRepo"
+            url = uri("https://repo.yamida.xyz/releases")
+            credentials {
+                username = System.getenv("YAMIDA_USERNAME") ?: error("YAMIDA_USERNAME environment variable is not set")
+                password = System.getenv("YAMIDA_TOKEN") ?: error("YAMIDA_TOKEN environment variable is not set")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "xyz.yamida"
+            artifactId = "jda-commander"
+            version = "1.0.2"
+            from(components["java"])
+        }
+    }
+}
+
+
+kotlin {
+    jvmToolchain(21)
+}
